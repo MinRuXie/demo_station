b22f8e8d5689353685e37ef8c3be8e5597953e8a
Joy <a556987@gmail.com>
2022-06-17 14:27:41 +0800

    update joyxie-portfolio

--- joyxie-portfolio/dist/assets/templates/container.js
+++ joyxie-portfolio/dist/assets/templates/container.js
@@ -1,1 +1,1 @@
-import Nav from"../templates/nav.js";const container='\n<div>\n    <Nav />\n    <transition name="fade">\n        <router-view></router-view>\n    </transition>\n</div>\n',Container={template:container,components:{Nav:Nav}};export default Container;
+import Nav from"../templates/nav.js";const container='\n<section>\n    <Nav />\n    <transition name="fade">\n        <router-view></router-view>\n    </transition>\n</section>\n',Container={template:container,components:{Nav:Nav}};export default Container;
